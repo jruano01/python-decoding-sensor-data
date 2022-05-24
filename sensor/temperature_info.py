7ba86b362549d2d96400df0c9e5183593fc6c453
jruano01 <101552889+jruano01@users.noreply.github.com>
2022-05-24 11:04:55 +0200

    Create temperature_info.py

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -1,40 +1,18 @@
-# Runner script for all modules
-from load_data import load_sensor_data
 from house_info import HouseInfo
-from datetime import datetime, date
-from temperature_info import TemperatureData
-from humidity_info import HumidityData
-##############################
-# Do not remove these two lines
-# They are needed to validate your unittest
-data = []
-print("Sensor Data App")
-##############################
+from datetime import date
 
-# Module 1 code here:
-data = load_sensor_data()
-print("Loaded records: {}".format(len(data)))
-# Module 2 code here:
-house_info = HouseInfo(data)
-test_area = 1
-recs = house_info.get_data_by_area("id", rec_area=test_area)
-print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+class TemperatureData(HouseInfo):
+    def _convert_data(self, data):
+        recs = []
+        for rec in data:
+            # Convert string of integers into integers based 10
+            recs.append(int(rec, base=10))
+        return recs
+    
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("temperature", rec_area)
+        return self._convert_data(recs)
 
-test_date = datetime.strptime("5/9/20", "%m/%d/%y")
-recs = house_info.get_data_by_date("id", rec_date=test_date)
-print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
-
-# Module 3 code here:
-temperature_data = TemperatureData(data)
-recs = temperature_data.get_data_by_area(rec_area=test_area)
-print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
-print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
-
-recs = temperature_data.get_data_by_date(rec_date=test_date)
-print("\nHouse Temperature sensor records for date: {} = {}".format(
-    test_date.strftime("%m/%d/%y"), len(recs)))
-print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
-
-# Module 4 code here:
-
-# Module 5 code here:
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("temperature", rec_date)
+        return self._convert_data(recs)
